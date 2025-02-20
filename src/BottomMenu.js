96236248f65e81e3ef26bb8e8f669ad44c6c3df5
66nightuper <lovu201@gmail.com>
2025-02-20 14:59:48 +0300

    No

--- src/BottomMenu.js
+++ src/BottomMenu.js
@@ -9,7 +9,7 @@ import './BottomMenu.css';
 
 const BottomMenu = () => {
     const { isDarkTheme, toggleTheme } = useContext(ThemeContext);
-    const [selected, setSelected] = useState('sayk');
+    const [selected, setSelected] = useState('Prodj');
     const [isLight, setIsLight] = useState(false); 
     const navigate = useNavigate(); 
 
